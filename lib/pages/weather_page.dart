d55336f9fb86a6d1a6e7987e33a61e50e852f110
TONMOY, WASIMUL BARI <punpunonodera754@gmail.com>
2025-04-13 10:09:01 +0600

    Initial commit

---
+++ lib/pages/weather_page.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:weather_app/models/weather_model.dart';
+import 'package:weather_app/services/weather_service.dart';
+
+class WeatherPage extends StatefulWidget {
+  const WeatherPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _WeatherPageState();
+}
+
+class _WeatherPageState extends State<WeatherPage> {
+  final _weatherService = WeatherService('908ea18cce3b1b0a8b21ad23afd5cd14');
+  Weather? _weather;
+
+  _fetchWeather() async {
+    String cityName = await _weatherService.getCurrentCity();
+
+    try {
+      final weather = await _weatherService.getWeather(cityName);
+
+      setState(() {
+        _weather = weather;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _fetchWeather();
+  }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     body: Column(
+  //       mainAxisAlignment: MainAxisAlignment.center,
+  //       children: [
+  //         Text(_weather?.cityName ?? "loading city.."),
+  //         Text("${_weather?.temperature.round()} + '°C'"),
+  //       ],
+  //     ),
+  //   );
+  // }
+
+  // weather animation
+  String getWeatherAnimation(String? mainCondition) {
+    if (mainCondition == null) return 'assets/sunny.json';
+
+    switch (mainCondition.toLowerCase()) {
+      case 'clouds':
+      case 'mist':
+      case 'smoke':
+      case 'haze':
+      case 'dust':
+      case 'fog':
+        return 'assets/clouddy.json';
+      case 'rain':
+      case 'drizzle':
+      case 'shower rain':
+        return 'assets/rain.json';
+      case 'thunderstrom':
+        return 'assets/thunder.json';
+      case 'clear':
+        return 'assets/sunny.json';
+      default:
+        return 'assets/sunny.json';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment:
+              CrossAxisAlignment.center, // Center text horizontally
+          children: [
+            if (_weather == null)
+              Text("Loading weather...")
+            else ...[
+              //
+              Text(_weather!.cityName),
+
+              //animation
+              Lottie.asset(getWeatherAnimation(_weather?.mainCondition)),
+
+              Text("${_weather!.temperature.round()}°C"),
+
+              //weather conditions
+              Text(_weather?.mainCondition ?? ""),
+            ],
+          ],
+        ),
+      ),
+    );
+  }
+}
